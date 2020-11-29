3fcc767b46049c3e748f6065c46e3fc3b0b4a2ae
Pawan Chaudhary <pawan.email.np>
2020-11-29 13:48:36 +0545

    Connected to the mongodb with database: feedbacks

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const mongoose = require('mongoose');
 const http = require('http');
 const app = require('./app');
 const port = process.env.PORT || 2000;
 const server = http.createServer(app);
-server.listen(port);
+const URI = 'mongodb+srv://pawan:pawan@cluster0.ht4y4.mongodb.net/<feedbacks>?retryWrites=true&w=majority'
+// mongoose.connect( URI || 'mongodb://localhost/new',{
+mongoose.connect( URI || 'mongodb://localhost/new',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', ()=> {
+    console.log('Mongoose is connected');
+});
+server.listen(port, console.log(`Server is running at ${port}`));
