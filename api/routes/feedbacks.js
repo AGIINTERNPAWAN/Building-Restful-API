513756b06b1aae87766321c6003711a74ec95b5a
Pawan Chaudhary <pawan.email.np>
2020-11-30 17:21:57 +0545

    No changes

--- api/routes/feedbacks.js
+++ api/routes/feedbacks.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Feedback = require('../model/feedback');
 
-
-
 router.get('/',(req,res) =>{
     Feedback.find({ }).then((feedbackData)=>{
         res.json(feedbackData);
